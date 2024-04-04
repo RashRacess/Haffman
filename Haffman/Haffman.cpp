b0e0c0d0a0adaa1525b288dd3b1ade4344cd6955
Artem Isachenkov <artemyt237@gmail.com>
2024-04-04 23:54:03 +0300

    refactring

--- Haffman/Haffman.cpp
+++ Haffman/Haffman.cpp
@@ -5,9 +5,9 @@ int main() {
 	SetConsoleOutputCP(1251);
 	string text = GetText();
 
-	std::map<char, int> frequencies = create_character_tree(text);
-	std::vector<Node> nodes = createArrFrequencies(frequencies);
-	Node root = huffman(nodes);
+	std::map<char, int> frequencies = create_character_tree(text); // частоты встречи каждого символа
+	std::vector<Node> nodes = createArrFrequencies(frequencies); // создание массива из Node
+	Node root = huffman(nodes); // создание дерева где корень root
 	map<char, string> codes = codesMap(frequencies, root);
 
 	string encodedString = getCodedString(codes, text);
